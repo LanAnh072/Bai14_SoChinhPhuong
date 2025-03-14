aaf4067b9b5914d805efc7a4523eaa5a68052f5d
SPC <SPC@29-LAB402>
2025-03-14 08:59:27 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Bai14_SoChinhPhuong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSo = new System.Windows.Forms.TextBox();
+            this.lblKQ = new System.Windows.Forms.Label();
+            this.btnKtra = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(281, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "KIỂM TRA SỐ CHÍNH PHƯƠNG";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Số";
+            // 
+            // txtSo
+            // 
+            this.txtSo.Location = new System.Drawing.Point(88, 84);
+            this.txtSo.Name = "txtSo";
+            this.txtSo.Size = new System.Drawing.Size(136, 26);
+            this.txtSo.TabIndex = 2;
+            // 
+            // lblKQ
+            // 
+            this.lblKQ.AutoSize = true;
+            this.lblKQ.Location = new System.Drawing.Point(53, 129);
+            this.lblKQ.Name = "lblKQ";
+            this.lblKQ.Size = new System.Drawing.Size(64, 20);
+            this.lblKQ.TabIndex = 3;
+            this.lblKQ.Text = "Kết quả";
+            // 
+            // btnKtra
+            // 
+            this.btnKtra.Location = new System.Drawing.Point(86, 163);
+            this.btnKtra.Name = "btnKtra";
+            this.btnKtra.Size = new System.Drawing.Size(138, 28);
+            this.btnKtra.TabIndex = 4;
+            this.btnKtra.Text = "Kiểm tra";
+            this.btnKtra.UseVisualStyleBackColor = true;
+            this.btnKtra.Click += new System.EventHandler(this.btnKtra_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 574);
+            this.Controls.Add(this.btnKtra);
+            this.Controls.Add(this.lblKQ);
+            this.Controls.Add(this.txtSo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSo;
+        private System.Windows.Forms.Label lblKQ;
+        private System.Windows.Forms.Button btnKtra;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai14_SoChinhPhuong
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKtra_Click(object sender, EventArgs e)
+        {
+            int so = Int32.Parse(txtSo.Text);
+            if (soChinhPhuong(so))
+            {
+                lblKQ.Text = so + " là số chính phương";
+            }
+            else
+            {
+                lblKQ.Text = so + " không phải là số chính phương";
+            }
+        }
+        bool soChinhPhuong(int so)
+        {
+            if (so < 0) return false;
+            int sqrt = (int)Math.Sqrt(so);
+            return sqrt * sqrt == so;
+        }
+    }
+}
